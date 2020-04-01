927eea2d893f60f97d7c2729841271cb97e4d28c
Mellieejj <Melanieverheul1992@gmail.com>
2020-04-01 17:08:14 +0200

    deleted comment out code i dont need

--- products/router.js
+++ products/router.js
@@ -10,20 +10,6 @@ router.get("/products", (request, response, next) => {
     .catch(next);
 });
 
-// router.get("/products/:productName", (request, response, next) => {
-//   Product.findOne(
-//     { where: { name: request.params.productName } },
-//     { include: [Categorie] }
-//   )
-//     .then(product => {
-//       if (!product) {
-//         response.status(404).end();
-//       } else {
-//         response.json(product);
-//       }
-//     })
-//     .catch(next);
-// });
 
 router.get("/products/:productId", (request, response, next) => {
   Product.findByPk(request.params.productId, {
