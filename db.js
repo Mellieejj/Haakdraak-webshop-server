a78f0aefc210d84b9eead2446a3f65e546a2cd9b
Mellieejj <Melanieverheul1992@gmail.com>
2020-04-02 21:36:54 +0200

    reset database

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const Sequelize = require("sequelize");
 
-const databaseURL = process.env.DATABASE_URL || "postgres://postgres:secret@localhost:5432/postgres";
+const databaseURL =
+  process.env.DATABASE_URL ||
+  "postgres://postgres:secret@localhost:5432/postgres";
 
 const db = new Sequelize(databaseURL);
 
-db.sync({ force: false })
+db.sync({ force: true })
   .then(() => console.log("database connected"))
   .catch(console.error);
 
