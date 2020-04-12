74bd5a94a3fd60d49f4c2b1988ca6a571b44ec7b
Mellieejj <Melanieverheul1992@gmail.com>
2020-04-12 15:07:24 +0200

    router for find all orders, router for find by pk

--- orders/router.js
+++ orders/router.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
 const Order = require("../orders/model");
+const OrderProducts = require("../orderProducts/model");
+const Product = require("../products/model");
 // const Product = require("../products/model")
 
 const router = Router();
@@ -14,7 +16,7 @@ router.post("/orders", async (request, response, next) => {
       housenr,
       postcode,
       city,
-      opmerkingen
+      opmerkingen,
     } = request.body.form;
 
     if (
@@ -29,11 +31,11 @@ router.post("/orders", async (request, response, next) => {
     ) {
       return response.status(400).send({
         message:
-          "Winkelwagen is leeg & het formulier is niet volledig ingevuld."
+          "Winkelwagen is leeg & het formulier is niet volledig ingevuld.",
       });
     } else if (request.body.items.length === 0) {
       return response.status(400).send({
-        message: "Winkelwagen is leeg!"
+        message: "Winkelwagen is leeg!",
       });
     } else if (
       !firstName ||
@@ -45,7 +47,7 @@ router.post("/orders", async (request, response, next) => {
       !city
     ) {
       return response.status(400).send({
-        message: "Formulier was niet volledig ingevuld"
+        message: "Formulier was niet volledig ingevuld",
       });
     } else {
       const newOrder = {
@@ -56,25 +58,54 @@ router.post("/orders", async (request, response, next) => {
         housenr: housenr,
         postcode: postcode,
         city: city,
-        opmerkingen: opmerkingen
+        opmerkingen: opmerkingen,
       };
       const order = await Order.create(newOrder);
 
       // console.log(request.body.items);
       response.status(200).send(
-        request.body.items.map(item =>
+        request.body.items.map((item) =>
           order.addProduct(item.id, {
-            through: { quantity: item.quantity }
+            through: { quantity: item.quantity },
           })
         )
       );
     }
   } catch (error) {
     response.status(500).send({
-      message: "U heeft niet alle gegevens ingevuld."
+      message: "U heeft niet alle gegevens ingevuld.",
     });
     // next(error);
   }
 });
+
+router.get("/orders", (request, response, next) => {
+  Order.findAll({ include: [Product] })
+    .then((order) => response.json(order))
+    .catch(next);
+});
+
+router.get("/orders/:orderId", async (request, response, next) => {
+  try {
+    const order = await Order.findByPk(request.params.orderId, {
+      include: [Product],
+    });
+    return response.send(order);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// router.get("/orders/:orderId", async (request, response, next) => {
+//   try {
+//     const list = await OrderProducts.findAll({
+//       where: { orderId: request.params.orderId },
+//     });
+
+//     return response.send(list);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
 
 module.exports = router;
