0a68ab71a72b171cbeac52aebb5feb61362f21eb
Mellieejj <Melanieverheul1992@gmail.com>
2020-05-11 15:20:04 +0200

    post router product, with images and categorie id

--- products/router.js
+++ products/router.js
@@ -6,16 +6,15 @@ const router = Router();
 
 router.get("/products", (request, response, next) => {
   Product.findAll({ include: [Categorie, ProductImage] })
-    .then(product => response.json(product))
+    .then((product) => response.json(product))
     .catch(next);
 });
 
-
 router.get("/products/:productId", (request, response, next) => {
   Product.findByPk(request.params.productId, {
-    include: [Categorie, ProductImage]
+    include: [Categorie, ProductImage],
   })
-    .then(product => {
+    .then((product) => {
       if (!product) {
         response.status(404).end();
       } else {
@@ -23,6 +22,59 @@ router.get("/products/:productId", (request, response, next) => {
       }
     })
     .catch(next);
+});
+
+router.post("/products", async (request, response, next) => {
+  try {
+    const {
+      name,
+      price,
+      description,
+      size,
+      optioneel,
+      stock,
+      categorie,
+    } = request.body.fields;
+    
+    const { productImages } = request.body;
+
+    if (!name || !price || !description || !size || !optioneel || !categorie) {
+      return response
+        .status(400)
+        .send({ message: "Niet alle velden zijn ingevuld." });
+    } else {
+      const categorie_id = await Categorie.findOne({
+        where: { name: categorie },
+      });
+
+      const newProduct = {
+        name: name,
+        price: price,
+        description: description,
+        size: size,
+        optioneel: optioneel,
+        stock: stock,
+        categorieId: categorie_id.id,
+      };
+
+      const product = await Product.create(newProduct);
+
+      await productImages.map((image) => {
+        const newImage = {
+          url: image.url,
+          thumbnail: Boolean(image.thumbnail),
+          productId: product.id,
+        };
+        const newProductImage = ProductImage.create(newImage);
+
+        return newProductImage;
+      });
+
+      response.status(200).send();
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
